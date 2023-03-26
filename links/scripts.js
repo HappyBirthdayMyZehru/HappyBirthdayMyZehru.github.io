e91eb9ced194c484a56d902a222949caade1c3dc
Hamza Bin Aamir <76187903+HamzaBAamir@users.noreply.github.com>
2023-03-26 17:31:56 +0500

    Transition

---
+++ links/scripts.js
@@ -0,0 +1,12 @@
+let animIndex = 0;
+//document.body.style.backgroundImage = "linear-gradient(to bottom right, #ff7300, #ffc400)";
+//document.body.style.overflow = "hidden";
+// background-image: linear-gradient(to bottom right, #ff7300, #ffc400);
+// background-size: 100%;
+// background-repeat: no-repeat;
+// overflow: hidden;
+
+
+setTimeout(function(){
+    animIndex += 1;
+},2000);
